f1e74c2c40cc898761da08aa31b7ad096010aa20
Edward Haas <edwardh@redhat.com>
2022-03-17 22:16:37 +0200

    member: Add the member table
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- member/tests.py
+++ member/tests.py
@@ -1,3 +1,43 @@
-# from django.test import TestCase
+# MIT License
+#
+# Copyright (c) 2022 Edward Haas
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-# Create your tests here.
+
+import pytest
+
+from . import models
+
+
+USERNAME = "ed"
+FIRSTNAME = "Edy"
+LASTNAME = "Haas"
+
+
+@pytest.fixture
+def member0():
+    return models.Member(username=USERNAME, firstname=FIRSTNAME, lastname=LASTNAME)
+
+
+class TestMemberModel:
+    def test_new_member(self, member0):
+        assert member0.username == USERNAME
+        assert member0.firstname == FIRSTNAME
+        assert member0.lastname == LASTNAME
